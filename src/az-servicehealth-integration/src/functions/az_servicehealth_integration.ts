5d090d9eda75f731c7eef8d756538f9daab06aea
wxz <weixianzhang@outlook.com>
2024-05-12 21:03:46 +0800

    feat: Added function error handling
    - config: Added config to resolve Typescript build error

--- src/az-servicehealth-integration/src/functions/az_servicehealth_integration.ts
+++ src/az-servicehealth-integration/src/functions/az_servicehealth_integration.ts
@@ -1,5 +1,5 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import { ClientSecretCredential, DefaultAzureCredential, EnvironmentCredential, EnvironmentCredentialOptions  } from "@azure/identity"
+import { ClientSecretCredential, DefaultAzureCredential, OnBehalfOfCredential   } from "@azure/identity"
 import { MicrosoftResourceHealth } from "@azure/arm-resourcehealth"
 import AppConfig from "./appconfig";
 
@@ -29,24 +29,43 @@ function initAzureCredential() {
 export async function az_servicehealth_integration(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     // context.log(`Http function processed request for url "${request.url}"`);
 
-    globalThis.appconfig = AppConfig.loadFromEnvVar()
+    try {
+        // globalThis.appconfig = AppConfig.loadFromEnvVar()
 
-    initAzureCredential()
+        // initAzureCredential()
 
-    const subscriptionId = "d8732e82-febd-4b92-b1ed-8fbce80a9ad8"
-    const credential = globalThis.techpassAzCred;
-    const client = new MicrosoftResourceHealth(new DefaultAzureCredential(), subscriptionId) ;
-    //const accessToken =  await credential.getToken("https://management.azure.com/user_impersonation")
+        const subscriptionId = "d8732e82-febd-4b92-b1ed-8fbce80a9ad8"
+        const credential = globalThis.techpassAzCred;
+        const client = new MicrosoftResourceHealth(new DefaultAzureCredential()) ;
+        //const accessToken =  await credential.getToken("https://management.azure.com/user_impersonation")
 
-    const resArray = new Array();
+        const resArray = new Array();
 
-    for await (let item of client.eventsOperations.listByTenantId(null)) {
-        resArray.push(item);
+        const iterator = client.eventsOperations.listByTenantId(null)
+        let next;
+
+        while ((next = await iterator.next()).done === false) {
+            resArray.push(next.value);
+        }
+
+        // for await (const item of client.eventsOperations.listByTenantId(null)) {
+            
+        // }
+
+        console.log(resArray);
+
+        return { body: `Hello, World}!` };
     }
-
-    console.log(resArray);
-
-    return { body: `Hello, World}!` };
+    catch(e){
+     
+        const errMsg = e.message; // error under useUnknownInCatchVariables
+        context.error(errMsg)
+        return {
+            status: 400,
+            body: errMsg
+        };
+    }
+    
 };
 
 app.http('az_servicehealth_integration', {
