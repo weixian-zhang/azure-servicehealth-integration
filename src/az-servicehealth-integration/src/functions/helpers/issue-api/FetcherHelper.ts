ed8a156ae73281d9a644421dfaa9bc9d00f91557
wxz <weixianzhang@outlook.com>
2024-05-22 22:40:11 +0800

    refactor: extract logic of getting issue data into DTO, into a dedicated class FetcherHelper
    In this way, both ApiIssueFetcher and MockIssueFetcher can share the same issue creation code

--- src/az-servicehealth-integration/src/functions/helpers/issue-api/FetcherHelper.ts
+++ src/az-servicehealth-integration/src/functions/helpers/issue-api/FetcherHelper.ts
@@ -1,7 +1,8 @@
-// import { ImpactUpdates, ImpactedService, ServiceIssue } from "./ServiceIssueModels";
-// import * as _ from 'lodash';
+import { ImpactUpdates, ImpactedResource, ImpactedService, ServiceIssue } from "./ServiceIssueModels";
+import * as _ from 'lodash';
 
 export default class FetcherHelper {
+    static regionToFilter = "Southeast Asia";
     static levelMap = {
         'Error': 'Widespread issues accessing multiple services across multiple regions are impacting a broad set of customers',
         'Warning': 'Issues accessing specific services and/or specific regions are impacting a subset of customers',
@@ -14,68 +15,120 @@ export default class FetcherHelper {
         }
         return '';
     }
+
+    //issue type is @azure/arm-resourcehealth/PagedAsyncIterableIterator<Event_2, Event_2[], PageSettings> or | any (mock Json data)
+    static createIssueInIssueBag(tenantName: string, currIssue: any, issueBag: Map<string, ServiceIssue>) {
+
+        const eventType = _.isEmpty(currIssue.eventType) ? currIssue.properties.eventType : currIssue.eventType;
+
+        if (eventType != 'ServiceIssue') {
+            return;
+        }
+        
+        let si = new ServiceIssue();
+
+        si.TenantName = tenantName;
+        si.TrackingId = currIssue.name;
+        si.OverallStatus = _.isEmpty(currIssue.status) ? currIssue.properties.status : currIssue.status;
+        si.Title = _.isEmpty(currIssue.title) ? currIssue.properties.title : currIssue.title;
+        si.Summary = _.isEmpty(currIssue.summary) ? currIssue.properties.summary : currIssue.summary;
+        si.Description = _.isEmpty(currIssue.description) ? currIssue.properties.description : currIssue.description;
+        si.ImpactStartTime = _.isEmpty(currIssue.impactStartTime) ? new Date(currIssue.properties.impactStartTime) : new Date(currIssue.impactStartTime);
+        si.ImpactMitigationTime = _.isEmpty(currIssue.impactMitigationTime) ? new Date(currIssue.properties.impactMitigationTime) : new Date(currIssue.impactMitigationTime);
+        si.LastUpdateTime = _.isEmpty(currIssue.lastUpdateTime) ? new Date(currIssue.properties.lastUpdateTime) : new Date(currIssue.lastUpdateTime);
+        si.LastUpdateTimeEpoch = si.LastUpdateTime.valueOf();
+        si.Level = _.isEmpty(currIssue.level) ? currIssue.properties.level : currIssue.level;
+        si.LevelDescription = FetcherHelper.getLevelDescription(si.Level);
+        si.ImpactedServices = new Array();
+        si.ImpactedResources = new Array();
+
+        const impact = _.isEmpty(currIssue.impact) ? currIssue.properties.impact : currIssue.impact;
+
+        impact.forEach(impact => {
+
+            //const regions = _.isEmpty(impact.impactedRegions) ? impact. : currIssue.impactedRegions;
+
+            impact.impactedRegions.forEach(region => {
+
+                if (region.impactedRegion == this.regionToFilter || region.impactedRegion == "Global") {
+
+                    const impactedSvc = new ImpactedService();
+
+                    impactedSvc.ImpactedService = impact.impactedService;
+                    impactedSvc.IsGlobal = (region.impactedRegion == "Global") ? true : false;
+                    impactedSvc.SEARegionOrGlobalStatus = region.status;
+                    impactedSvc.SEARegionOrGlobalLastUpdateTime = new Date(region.lastUpdateTime);
+                    impactedSvc.ImpactedTenants = region.impactedTenants;
+                    impactedSvc.ImpactedSubscriptions = region.impactedSubscriptions;
+                    impactedSvc.ImpactUpdates = new Array();
+                    
+                    if (!_.isEmpty(region.updates)) {
+
+                        for (const u of region.updates) {
+
+                            const iu = new ImpactUpdates();
+                            iu.Summary = u.summary;
+                            iu.UpdateDateTime = new Date(u.updateDateTime);
+                            iu.UpdateEpoch = iu.UpdateDateTime.valueOf();
+
+                            impactedSvc.ImpactUpdates.push(iu);
+                        }
+                    }
+                    
+                    si.ImpactedServices.push(impactedSvc);
+                }
+
+            }); // end foreach region
+
+        }); // end foreach impact
+        
+        // only include an issue when there is a service that is impacted Globally or in SEA region
+        // ignore issue that are non-global or other regions
+        if (_.isEmpty(si.ImpactedServices)) {
+            return;
+        }
+
+        // is previously collected issue
+        if (si.TrackingId in issueBag) {
+
+            FetcherHelper.groupImpactedServicesByTrackingId(si, issueBag);
+        }
+        else
+        {
+            issueBag.set(si.TrackingId, si);
+        }
+        
+    }
+
+    //as List by Subscription Id is called multiple by the number of subscription Id this app's service principal has access to
+    //issues retrieved will have duplicates, but the impacted services could be different as different services exist in different subscriptions
+    // this function groups up or merge impacted services by same tracking id.
+    static groupImpactedServicesByTrackingId(currIssue: ServiceIssue, serviceIssues: Map<string, ServiceIssue>) {
+
+        currIssue.ImpactedServices.forEach(cisvc => {
+           
+            const prevImpactedServices: ImpactedService[] = serviceIssues[currIssue.TrackingId].ImpactedServices;
+    
+            const index = prevImpactedServices.findIndex((previsvc) => previsvc.ImpactedService == cisvc.ImpactedService)
+    
+            if (index == -1) {
+                serviceIssues[currIssue.TrackingId].ImpactedServices.push(cisvc);
+            }
+    
+        })
+    }
+
+    static createImpactedResourceInIssueBag
+        (trackingId: string, subscriptionId: string, resource: any, issueBag: Map<string, ServiceIssue>) {
+        const ir = new ImpactedResource();
+        ir.Id = resource.id;
+        ir.SubscriptionId =  subscriptionId;
+        ir.ResourceGroup = resource.resourceGroup;
+        ir.ResourceType = resource.targetResourceType;
+        ir.ResourceName =  resource.resourceName;
+        issueBag[trackingId].ImpactedResources.push(ir);
+    }
 }
 
-//     //issue type is @azure/arm-resourcehealth/Event_2
-//     static createServiceIssue(tenantName: string, issue: any) : [boolean, ServiceIssue] {
-        
-//         if (issue.eventType != 'ServiceIssue') {
-//             return [false, null]
-//         }
 
-//         let si = new ServiceIssue();
 
-//         si.TenantName = tenantName;
-//         si.TrackingId = issue.name
-//         si.Status = issue.status
-//         si.Title = issue.title
-//         si.Summary = issue.summary
-//         si.Description =issue.description
-//         si.ImpactStartTime = issue.impactStartTime;
-//         si.ImpactMitigationTime = issue.impactMitigationTime;
-//         si.LastUpdateTime = new Date(issue.lastUpdateTime);
-//         si.LastUpdateTimeEpoch = si.LastUpdateTime.valueOf();
-//         si.ImpactedServices = new Array();
-//         si.ImpactedResources = new Array();
-
-//         issue.impact.forEach(impact => {
-
-//             impact.impactedRegions.forEach(region => {
-
-//                 //** to enable after testing
-//                 //if (region.impactedRegion == this.regionToFilter || region.impactedRegion == "Global") {
-
-//                     const impactedSvc = new ImpactedService();
-
-//                     impactedSvc.ImpactedService = impact.impactedService;
-//                     impactedSvc.SoutheastAsiaRegionStatus = region.status;
-//                     impactedSvc.ImpactedTenants = region.impactedTenants;
-//                     impactedSvc.ImpactedSubscriptions = region.impactedSubscriptions;
-//                     impactedSvc.LastUpdateTime = region.lastUpdateTime;
-//                     impactedSvc.ImpactUpdates = new Array();
-                    
-//                     if (!_.isEmpty(region.updates)) {
-
-//                         for (const u of region.updates) {
-
-//                             const iu = new ImpactUpdates();
-//                             iu.Summary = u.summary;
-//                             iu.UpdateDateTime = new Date(u.updateDateTime);
-//                             iu.UpdateEpoch = iu.UpdateDateTime.valueOf();
-
-//                             impactedSvc.ImpactUpdates.push(iu);
-//                         }
-//                     }
-                    
-
-//                     si.ImpactedServices.push(impactedSvc);
-
-//                 //}
-
-//             });
-
-//         });
-
-//         return [true, issue]
-//     }
-// }
--- src/az-servicehealth-integration/src/functions/helpers/issue-api/MockIssueFetcher.ts
+++ src/az-servicehealth-integration/src/functions/helpers/issue-api/MockIssueFetcher.ts
@@ -6,19 +6,22 @@ import * as _ from 'lodash';
 export default class MockIssueGenerator implements IIssueFetcher {
     wogTenantName = "WOG";
     techpassTenantName = "TechPass";
-    dataPath: string = "src/functions/helpers/issue-api/mock-data/sea_issues_only.json";
+    issuesDataPath: string = "src/functions/helpers/issue-api/mock-data/sea_issues_only.json";
+    impactedResourcesDataPath: string = "src/functions/helpers/issue-api/mock-data/impacted_resources.json";
     regionToFilter = "Southeast Asia";
     
     //reuse ApiIssueFetcher and add/edit/remove issue data
     async fetchIssuesAndImpactedResources(): Promise<ServiceIssue[]> {
 
-        const data = await fs.promises.readFile(this.dataPath, "utf8");
+        let issueBag = new Map<string, ServiceIssue>();
 
-        let issues = JSON.parse(data) as any[];
+        const issues = await this.listIssuesBySubscriptionIds();
         
         let curr: number = 0;
         const numOfIssuesToReturn: number = 10;
+
         const result = new Array<ServiceIssue>();
+        let serviceIssues = new Map<string, ServiceIssue>();
 
         for (const issue of issues) {
 
@@ -26,77 +29,133 @@ export default class MockIssueGenerator implements IIssueFetcher {
                 return Promise.resolve(result);
             }
 
-            if (issue.properties.eventType != 'ServiceIssue') {
+            const trackingId = issue.name;
+
+            FetcherHelper.createIssueInIssueBag(wogTenantName, issue, issueBag);
+            
+            if (issueBag.has(trackingId)) {
+                // get impacted resources
+                await this.fetchImpactedResourcesForIssue(trackingId, issueBag);
+            }
+
+            // if (issue.properties.eventType != 'ServiceIssue') {
+            //     continue;
+            // }
+            
+            // const [seaRegionImpacted, si] = FetcherHelper.createServiceIssue(wogTenantName, issue);
+
+            // if (!seaRegionImpacted) {
+            //     continue;
+            // }
+        
+            // // is previously collected issue
+            // if (si.TrackingId in serviceIssues) {
+
+            //     FetcherHelper.groupImpactedServicesByTrackingId(si, serviceIssues);
+            // }
+            // else
+            // {
+            //     serviceIssues.set(si.TrackingId, si);
+            // }
+
+            curr++;
+        }
+
+            // let si = new ServiceIssue();
+
+            // si.TenantName = 'WOG';
+            // si.TrackingId = issue.name
+            // si.OverallStatus = issue.properties.status
+            // si.Title = issue.properties.title
+            // si.Summary = issue.properties.summary
+            // si.Description =issue.properties.description
+            // si.ImpactStartTime = issue.properties.impactStartTime;
+            // si.ImpactMitigationTime = issue.properties.impactMitigationTime;
+            // si.LastUpdateTime = new Date(issue.properties.lastUpdateTime);
+            // si.LastUpdateTimeEpoch = si.LastUpdateTime.valueOf();
+            // si.Level = issue.properties.Level;
+            // si.LevelDescription = FetcherHelper.getLevelDescription(si.Level);
+            // si.ImpactedServices = new Array();
+            // si.ImpactedResources = new Array();
+
+            // issue.properties.impact.forEach(impact => {
+
+            //     impact.impactedRegions.forEach(region => {
+
+            //         if (region.impactedRegion == this.regionToFilter || region.impactedRegion == "Global") {
+
+            //             const impactedSvc = new ImpactedService();
+
+            //             impactedSvc.ImpactedService = impact.impactedService;
+            //             impactedSvc.IsGlobal = (region.impactedRegion == "Global") ? true : false;
+            //             impactedSvc.SEARegionOrGlobalStatus = region.status;
+            //             impactedSvc.SEARegionOrGlobalLastUpdateTime = new Date(region.lastUpdateTime);
+            //             impactedSvc.ImpactedTenants = region.impactedTenants;
+            //             impactedSvc.ImpactedSubscriptions = region.impactedSubscriptions;
+                        
+            //             impactedSvc.ImpactUpdates = new Array();
+                        
+            //             if (!_.isEmpty(region.updates)) {
+
+            //                 for (const u of region.updates) {
+
+            //                     const iu = new ImpactUpdates();
+            //                     iu.Summary = u.summary;
+            //                     iu.UpdateDateTime = new Date(u.updateDateTime);
+            //                     iu.UpdateEpoch = iu.UpdateDateTime.valueOf();
+    
+            //                     impactedSvc.ImpactUpdates.push(iu);
+            //                 }
+            //             }
+                        
+            //             si.ImpactedServices.push(impactedSvc);
+
+            //         }
+
+            //     });
+
+            // });
+
+            // // only include an issue when there is one or more SEA region impacted services 
+            // if (!_.isEmpty(si.ImpactedServices)) {
+            //     result.push(si);
+            // }
+
+            
+
+        // const r = new Array();
+        // r.push(result[0])
+        // return Promise.resolve(r);
+    }
+
+    private async fetchImpactedResourcesForIssue(trackingId: string, issueBag: Map<string, ServiceIssue>) {
+            
+        const data = await fs.promises.readFile(this.impactedResourcesDataPath, "utf8");
+        let ips = JSON.parse(data) as any[];
+        ips = ips['value'];
+
+        for (let ip of ips) {//this.resourceHealthClient.impactedResources.listByTenantIdAndEventId(issue.TrackingId)) {
+
+            if (_.isEmpty(ip.properties.targetResourceId)) {
                 continue;
             }
 
-            let si = new ServiceIssue();
+            const ir = new ImpactedResource();
+            const rArr: any[] = ip.properties.targetResourceId.split("/");
+            ir.SubscriptionId =  rArr[1];
+            ir.ResourceGroup = rArr.reverse()[4];
+            ir.ResourceType = _.isEmpty(ip.properties.targetResourceType) ? '' : ip.properties.targetResourceType;
+            ir.ResourceName =  _.last(rArr);
+            
+            issueBag[trackingId].ImpactedResources.push(ir);
+            //issue.ImpactedResources.push(ir);
+        }
+    }
 
-            si.TenantName = 'WOG';
-            si.TrackingId = issue.name
-            si.OverallStatus = issue.properties.status
-            si.Title = issue.properties.title
-            si.Summary = issue.properties.summary
-            si.Description =issue.properties.description
-            si.ImpactStartTime = issue.properties.impactStartTime;
-            si.ImpactMitigationTime = issue.properties.impactMitigationTime;
-            si.LastUpdateTime = new Date(issue.properties.lastUpdateTime);
-            si.LastUpdateTimeEpoch = si.LastUpdateTime.valueOf();
-            si.Level = issue.properties.Level;
-            si.LevelDescription = FetcherHelper.getLevelDescription(si.Level);
-            si.ImpactedServices = new Array();
-            si.ImpactedResources = new Array();
-
-            issue.properties.impact.forEach(impact => {
-
-                impact.impactedRegions.forEach(region => {
-
-                    if (region.impactedRegion == this.regionToFilter || region.impactedRegion == "Global") {
-
-                        const impactedSvc = new ImpactedService();
-
-                        impactedSvc.ImpactedService = impact.impactedService;
-                        impactedSvc.IsGlobal = (region.impactedRegion == "Global") ? true : false;
-                        impactedSvc.SEARegionOrGlobalStatus = region.status;
-                        impactedSvc.SEARegionOrGlobalLastUpdateTime = new Date(region.lastUpdateTime);
-                        impactedSvc.ImpactedTenants = region.impactedTenants;
-                        impactedSvc.ImpactedSubscriptions = region.impactedSubscriptions;
-                        
-                        impactedSvc.ImpactUpdates = new Array();
-                        
-                        if (!_.isEmpty(region.updates)) {
-
-                            for (const u of region.updates) {
-
-                                const iu = new ImpactUpdates();
-                                iu.Summary = u.summary;
-                                iu.UpdateDateTime = new Date(u.updateDateTime);
-                                iu.UpdateEpoch = iu.UpdateDateTime.valueOf();
-    
-                                impactedSvc.ImpactUpdates.push(iu);
-                            }
-                        }
-                        
-                        si.ImpactedServices.push(impactedSvc);
-
-                    }
-
-                });
-
-            });
-
-            // only include an issue when there is one or more SEA region impacted services 
-            if (!_.isEmpty(si.ImpactedServices)) {
-                result.push(si);
-            }
-
-            curr++;
-        };
-
-        const r = new Array();
-        r.push(result[0])
-        return Promise.resolve(r);
+    private async listIssuesBySubscriptionIds() : Promise<any[]> {
+        const data = await fs.promises.readFile(this.issuesDataPath, "utf8");
+        let issues = JSON.parse(data) as any[];
+        return issues;
     }
     
-
 }
