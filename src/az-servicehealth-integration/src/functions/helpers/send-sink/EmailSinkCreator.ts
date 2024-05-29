bb846c5fe3e5dc9e9709b2f9eb8913c1356c9c1f
wxz <weixianzhang@outlook.com>
2024-05-29 20:49:00 +0800

    feat: AppConfig loads email config from env variables
    feat: both email sinks supports multiple recipient addresses

---
+++ src/az-servicehealth-integration/src/functions/helpers/send-sink/EmailSinkCreator.ts
@@ -0,0 +1,13 @@
+import AppConfig from "../AppConfig";
+import AzureCommServiceEmailSink from "./AzureCommServiceEmailSink";
+import { IEmailSink } from "./IEmailSink";
+import NodeEmailSink from "./NodeMailerEmailSink";
+
+export default class EmailSinkCreator {
+    static create(appconfig: AppConfig) : IEmailSink {
+        if (appconfig.IsDevTest) {
+            return new AzureCommServiceEmailSink(appconfig);
+        }
+        return new NodeEmailSink(appconfig);
+    }
+}
