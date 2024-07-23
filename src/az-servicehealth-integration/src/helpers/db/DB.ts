d6e3b8eb1a506d22d636e68d49c5019494be6454
wxz <weixianzhang@outlook.com>
2024-07-23 21:49:20 +0800

    refactor: DB.ts now uses Storage Table as DB

--- src/az-servicehealth-integration/src/helpers/db/DB.ts
+++ src/az-servicehealth-integration/src/helpers/db/DB.ts
@@ -1,30 +1,34 @@
-import sqlite3 from 'sqlite3';
-import { open, Database } from 'sqlite'
+import { TableClient, TableServiceClient, TableEntity} from "@azure/data-tables"; '@azure/data-table';
+import { DefaultAzureCredential } from "@azure/identity";
 import * as _ from 'lodash';
 import { ServiceIssue } from '../issue-api/ServiceIssueModels';
+import { url } from "inspector";
 
 // status reference
 // https://learn.microsoft.com/en-us/rest/api/resourcehealth/events/list-by-subscription-id?view=rest-resourcehealth-2022-10-01&tabs=HTTP#eventstatusvalues
-export class TrackedIssue{
+export class TrackedIssue {
+
     public TenantName: string;
     public TrackingId: string;
-    public ImpactedService: string;
-    public LastUpdateTime: number;//unix epoch
+    public ImpactedServices: TrackedImpactedService[];
+    public LastUpdateTimeEpoch: number;//unix epoch
     public Status: string ;       // Active or Resolved
 
-    constructor(tenantName: string, trackingId: string, impactedService: string, lastUpdateTime: number, status: string) {
+    constructor(tenantName: string, trackingId: string, impactedServices: TrackedImpactedService[], lastUpdateTime: number, status: string) {
         this.TenantName = tenantName;
         this.TrackingId = trackingId;
-        this.ImpactedService = impactedService;
-        this.LastUpdateTime = lastUpdateTime;
+        this.ImpactedServices = impactedServices;
+        this.LastUpdateTimeEpoch = lastUpdateTime;
         this.Status = status;
     }
 }
 
 export class TrackedImpactedService {
+    Name: string;
     LastUpdateTime: number;
     Status: string
-    constructor(lastUpdateTime: number, status: string) {
+    constructor(name: string, lastUpdateTime: number, status: string) {
+        this.Name = name;
         this.LastUpdateTime = lastUpdateTime;
         this.Status = status;
     }
@@ -34,9 +38,7 @@ export class TrackedImpactedService {
 // https://github.com/typicode/lowdb
 export class DB {
 
-    private db: Database; //Low<{issues: Issue[];}>;
-    
-    private dbPath = __dirname + '\\db.sqlite';//process.cwd() + "\\src\\functions\\helpers\\db\\db.sqlite";
+    private tableClient: TableClient;
 
     constructor() {
         this.initDB();
@@ -44,85 +46,114 @@ export class DB {
 
     private async initDB() {
 
-        const createTableIssue = `
-            CREATE TABLE IF NOT EXISTS Issue (
-                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Tenant_Name TEXT NOT NULL,
-                Tracking_Id TEXT NOT NULL,
-                LastUpdateTime INTEGER NOT NULL,
-                Status TEXT NOT NULL
-            );
-        `;
+        const tableName = 'Issue';
+        const tableEndpoint = `${process.env.AZURE_STORAGETABLE_RESOURCEENDPOINT}`;
+        const credential = new DefaultAzureCredential();
 
-        const createTableImpactedService = `
+        const tableService = new TableServiceClient(
+            tableEndpoint,
+            credential
+            );
+        
+        // creates table if not exist
+        await tableService.createTable(tableName);
+        
+        this.tableClient = new TableClient(tableEndpoint, tableName, credential);
+        // const createTableIssue = `
+        //     CREATE TABLE IF NOT EXISTS Issue (
+        //         Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //         Tenant_Name TEXT NOT NULL,
+        //         Tracking_Id TEXT NOT NULL,
+        //         LastUpdateTime INTEGER NOT NULL,
+        //         Status TEXT NOT NULL
+        //     );
+        // `;
+
+        // const createTableImpactedService = `
             
-            CREATE TABLE IF NOT EXISTS Impacted_Service (
-                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                TrackingId TEXT NOT NULL,
-                ImpactedService TEXT NOT NULL,
-                LastUpdateTime INTEGER NOT NULL,
-                Status TEXT NOT NULL
-            );
-        `;
-        
-        this.db = await open({
-            filename: this.dbPath,
-            driver: sqlite3.Database
-        });
-        
-        await this.db.run(createTableIssue);
-
-        await this.db.run(createTableImpactedService);
+        //     CREATE TABLE IF NOT EXISTS Impacted_Service (
+        //         Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //         TrackingId TEXT NOT NULL,
+        //         ImpactedService TEXT NOT NULL,
+        //         LastUpdateTime INTEGER NOT NULL,
+        //         Status TEXT NOT NULL
+        //     );
+        // `;
+            
     }
 
     async issueExist(trackingId: string) : Promise<[boolean, TrackedIssue]> {
         try {
 
-            const existingIssue = await this.db.get(`
-                SELECT 
-                    TenantName,
-                    TrackingId,
-                    LastUpdateTime,
-                    Status 
-                FROM Issue
-                WHERE TrackingId = '${trackingId}';
-            `);
+            const entity: any = await this.tableClient.getEntity(trackingId, "");
 
-            if (!_.isEmpty(existingIssue)) {
-                return [true, existingIssue];
-            }
+            const issue = JSON.parse(entity.issue)
 
-            return [false, null];
-
+            return [true, issue];
 
         } catch (error) {
+            if (!_.isNil(error.statusCode) && error.statusCode == 404) {
+                return [false, null];
+            }
             throw new Error(`Error at DB.issueExist: ${error.message}`);
         }
+            // const existingIssue = await this.db.get(`
+            //     SELECT 
+            //         TenantName,
+            //         TrackingId,
+            //         LastUpdateTime,
+            //         Status 
+            //     FROM Issue
+            //     WHERE TrackingId = '${trackingId}';
+            // `);
+
+            // if (!_.isEmpty(existingIssue)) {
+            //     return [true, existingIssue];
+            // }
+
+            // return [false, null];
+
+
+        
     }
 
     async getImpactedServices(trackingId: string) : Promise<Map<string, TrackedImpactedService>> {
         try {
+            
+            const [exist, issue] = await this.issueExist(trackingId);
 
-            const impactedServices: TrackedIssue[] = await this.db.all(`
-                SELECT 
-                    ImpactedService,
-                    LastUpdateTime,
-                    Status 
-                FROM Impacted_Service
-                WHERE TrackingId = '${trackingId}';
-            `);
-
-            if (!_.isEmpty(impactedServices)) {
-
-                const map = new Map<string, TrackedImpactedService>();
-                impactedServices.forEach(isvc => {
-                    map.set(isvc.ImpactedService, new TrackedImpactedService(isvc.LastUpdateTime, isvc.Status))
-                });
-
-                return map;
+            if (!exist) {
+                return new Map<string, TrackedImpactedService>();
             }
 
-            return new Map<string, TrackedImpactedService>;
+            const map = new Map<string, TrackedImpactedService>();
+
+            issue.ImpactedServices.forEach((is) => {
+                map.set(is.Name,  is)
+            });
+
+            return map;
+
+            // const impactedServices: TrackedIssue[] = await this.db.all(`
+            //     SELECT 
+            //         ImpactedService,
+            //         LastUpdateTime,
+            //         Status 
+            //     FROM Impacted_Service
+            //     WHERE TrackingId = '${trackingId}';
+            // `);
+
+            // if (!_.isEmpty(impactedServices)) {
+
+            //     const map = new Map<string, TrackedImpactedService>();
+            //     impactedServices.forEach(isvc => {
+            //         map.set(isvc.ImpactedService, new TrackedImpactedService(isvc.LastUpdateTime, isvc.Status))
+            //     });
+
+            //     return map;
+            // }
+
+            // return new Map<string, TrackedImpactedService>;
 
 
         } catch (error) {
@@ -131,81 +162,82 @@ export class DB {
     }
 
 
-
-    //exist and status is Active
-    // async getImpactedServiceLastUpdateTime(trackingId: string) : Promise<[boolean, number[]]> {
-    //     try {
-
-    //         const serviceLastUpdateTimes = await this.db.get(`
-    //             SELECT ImpactedService, LastUpdateTime FROM Impacted_Service
-    //             WHERE TrackingId = '${trackingId}';`);
-
-    //         if (!_.isEmpty(serviceLastUpdateTimes)) {
-    //             return [true, serviceLastUpdateTimes];
-    //         }
-
-    //         return [false, []];
-
-
-    //     } catch (error) {
-    //         throw new Error(`Error at DB.issueExist: ${error.message}`);
-    //     } 
-    // }
-
-    // async getImpactedServiceStatus(trackingId: string, impactedService: string) : Promise<[boolean, Issue]> {
-    //     try {
-
-    //         const existingIssue = await this.db.get(`
-    //             SELECT ImpactedService, Status 
-    //             FROM Impacted_Service WHERE TrackingId = '${trackingId}' AND ImpactedService = '${impactedService}' `);
-
-    //         if (!_.isEmpty(existingIssue)) {
-    //             return [true, existingIssue];
-    //         }
-
-    //         return [false, null];
-
-
-    //     } catch (error) {
-    //         throw new Error(`Error at DB.issueExist: ${error.message}`);
-    //     } 
-    // }
-
-    async addIssue(issue: ServiceIssue) {
+    async addIssue(si: ServiceIssue) {
         try {
 
-                // save impacted services
-                for (const isvc of issue.ImpactedServices) {
-                    await this.db.run(`
-                        INSERT INTO Impacted_Service (
-                            TrackingId,
-                            ImpactedService,
-                            LastUpdateTime,
-                            Status
-                        )
-                        VALUES(
-                            '${issue.TrackingId}',
-                            '${isvc.ImpactedService}',
-                            '${isvc.SEARegionOrGlobalLastUpdateTime.valueOf()}',
-                            '${isvc.SEARegionOrGlobalStatus}'
-                        )`
-                    );
+                const impactedServices = [];
+
+                for (const isvc of si.ImpactedServices) {
+
+                    const is = new TrackedImpactedService(
+                        isvc.ImpactedService,
+                        isvc.SEARegionOrGlobalLastUpdateTime.valueOf(), 
+                        isvc.SEARegionOrGlobalStatus);
+
+                    impactedServices.push(is);
+                    // await this.db.run(`
+                    //     INSERT INTO Impacted_Service (
+                    //         TrackingId,
+                    //         ImpactedService,
+                    //         LastUpdateTime,
+                    //         Status
+                    //     )
+                    //     VALUES(
+                    //         '${issue.TrackingId}',
+                    //         '${isvc.ImpactedService}',
+                    //         '${isvc.SEARegionOrGlobalLastUpdateTime.valueOf()}',
+                    //         '${isvc.SEARegionOrGlobalStatus}'
+                    //     )`
+                    // );
                 };
 
-                await this.db.run(`
-                    INSERT INTO Issue (
-                        TenantName,
-                        TrackingId,
-                        LastUpdateTime,
-                        Status
-                    )
-                    VALUES(
-                        '${issue.TenantName}',
-                        '${issue.TrackingId}',
-                        '${issue.LastUpdateTime.valueOf()}',
-                        '${issue.OverallStatus}'
-                    )`
-                );
+                const ti = new TrackedIssue(
+                    si.TenantName, 
+                    si.TrackingId, 
+                    impactedServices, 
+                    si.LastUpdateTime.valueOf(),
+                    si.OverallStatus);
+
+                const tij = JSON.stringify(ti)
+
+                const resp = await this.tableClient.upsertEntity({
+                    partitionKey: si.TrackingId,
+                    rowKey: '',
+                    issue: tij
+                }, "Replace")
+
+                // save impacted services
+                // for (const isvc of issue.ImpactedServices) {
+                //     await this.db.run(`
+                //         INSERT INTO Impacted_Service (
+                //             TrackingId,
+                //             ImpactedService,
+                //             LastUpdateTime,
+                //             Status
+                //         )
+                //         VALUES(
+                //             '${issue.TrackingId}',
+                //             '${isvc.ImpactedService}',
+                //             '${isvc.SEARegionOrGlobalLastUpdateTime.valueOf()}',
+                //             '${isvc.SEARegionOrGlobalStatus}'
+                //         )`
+                //     );
+                // };
+
+                // await this.db.run(`
+                //     INSERT INTO Issue (
+                //         TenantName,
+                //         TrackingId,
+                //         LastUpdateTime,
+                //         Status
+                //     )
+                //     VALUES(
+                //         '${issue.TenantName}',
+                //         '${issue.TrackingId}',
+                //         '${issue.LastUpdateTime.valueOf()}',
+                //         '${issue.OverallStatus}'
+                //     )`
+                // );
 
         } catch (error) {
             throw new Error(`Error at DB.addIssue: ${error.message}`);
@@ -215,14 +247,25 @@ export class DB {
     async updateIssueResolved(trackingId: string, lastUpdatedTime: Date) {
         
         try {
-                await this.db.run(`
-                    UPDATE Issue
-                    SET
-                        Status = 'Resolved',
-                        LastUpdateTime = ${lastUpdatedTime.valueOf()}
-                    WHERE TrackingId = '${trackingId}';
-                `
-                );
+                const issue: TrackedIssue = await this.tableClient.getEntity(trackingId, "");
+                issue.Status= 'Resolved';
+                issue.LastUpdateTimeEpoch = lastUpdatedTime.valueOf();
+
+                const ij = JSON.stringify(issue)
+
+                await this.tableClient.upsertEntity({
+                    partitionKey: issue.TrackingId,
+                    rowKey: '',
+                    issue: ij
+                }, 'Replace')
+                // await this.db.run(`
+                //     UPDATE Issue
+                //     SET
+                //         Status = 'Resolved',
+                //         LastUpdateTime = ${lastUpdatedTime.valueOf()}
+                //     WHERE TrackingId = '${trackingId}';
+                // `
+                // );
 
         } catch (error) {
             throw new Error(`Error at DB.addIssue: ${error.message}`);
@@ -235,12 +278,29 @@ export class DB {
     async updateImpactedServiceLastUpdateTime(trackingId: string, impactedService: string, lastUpdatedTime: Date) {
         
         try {
-                await this.db.run(`
-                    UPDATE Impacted_Service
-                    SET LastUpdateTime = ${lastUpdatedTime.valueOf()}
-                    WHERE TrackingId = '${trackingId}' AND ImpactedService = '${impactedService}'
-                `
-                );
+                const issue: TrackedIssue = await this.tableClient.getEntity(trackingId, "");
+
+                for(const is of issue.ImpactedServices) {
+                    if (is.Name == impactedService) {
+                        is.LastUpdateTime = lastUpdatedTime.valueOf();
+                        break;
+                    }
+                }
+
+                const ij = JSON.stringify(issue)
+
+                await this.tableClient.upsertEntity({
+                    partitionKey: issue.TrackingId,
+                    rowKey: '',
+                    issue: ij
+                }, 'Replace')
+
+                // await this.db.run(`
+                //     UPDATE Impacted_Service
+                //     SET LastUpdateTime = ${lastUpdatedTime.valueOf()}
+                //     WHERE TrackingId = '${trackingId}' AND ImpactedService = '${impactedService}'
+                // `
+                // );
 
         } catch (error) {
             throw new Error(`Error at DB.addIssue: ${error.message}`);
@@ -250,14 +310,32 @@ export class DB {
     async updateImpactedServiceResolved(trackingId: string, impactedService: string, lastUpdatedTime: Date) {
 
         try {
-                await this.db.run(`
-                    UPDATE Impacted_Service
-                    SET 
-                        Status = 'Resolved',
-                        LastUpdateTime = ${lastUpdatedTime.valueOf()}
-                    WHERE TrackingId = '${trackingId}' AND ImpactedService = '${impactedService}'
-                `
-                );
+                const issue: TrackedIssue = await this.tableClient.getEntity(trackingId, "");
+
+                for(const is of issue.ImpactedServices) {
+                    if (is.Name == impactedService) {
+                        is.Status = 'Resolved'
+                        is.LastUpdateTime = lastUpdatedTime.valueOf();
+                        break;
+                    }
+                }
+
+                const ij = JSON.stringify(issue)
+
+                await this.tableClient.upsertEntity({
+                    partitionKey: issue.TrackingId,
+                    rowKey: '',
+                    issue: ij
+                }, 'Replace')
+
+                // await this.db.run(`
+                //     UPDATE Impacted_Service
+                //     SET 
+                //         Status = 'Resolved',
+                //         LastUpdateTime = ${lastUpdatedTime.valueOf()}
+                //     WHERE TrackingId = '${trackingId}' AND ImpactedService = '${impactedService}'
+                // `
+                // );
 
         } catch (error) {
             throw new Error(`Error at DB.addIssue: ${error.message}`);
