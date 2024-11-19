ef515b2d970c0cc7d3da44de53e7ac465222f19a
wxz <weixianzhang@outlook.com>
2024-11-19 15:10:35 +0800

    test: fix mocked MicrosoftResourceHealth able to set property subscriptionId without error

--- src/az-servicehealth-integration/src/helpers/issue-api/ApiIssueFetcher_IssueFilterer.test.ts
+++ src/az-servicehealth-integration/src/helpers/issue-api/ApiIssueFetcher_IssueFilterer.test.ts
@@ -5,7 +5,10 @@ import path from 'path'
 import AppConfig from '../AppConfig';
 import dotenv from 'dotenv'; 
 import { ClientSecretCredential } from "@azure/identity";
-import { MicrosoftResourceHealth, Event, EventsOperations, EventsListBySubscriptionIdOptionalParams, EventsListBySingleResourceOptionalParams, EventsListByTenantIdOptionalParams } from "@azure/arm-resourcehealth"
+import { MicrosoftResourceHealth, Event, EventsOperations, 
+    EventsListBySubscriptionIdOptionalParams, 
+    EventsListBySingleResourceOptionalParams, 
+    EventsListByTenantIdOptionalParams } from "@azure/arm-resourcehealth"
 import {PageSettings, PagedAsyncIterableIterator } from '@azure/core-paging/dist/commonjs/models';
 import { mock, when, instance } from 'ts-mockito';
 import test_data_case_1 from './test-data/unit-test-data/test_case_1.json';
@@ -22,8 +25,10 @@ class EventIterator implements PagedAsyncIterableIterator<Event, Event[], PageSe
         if (this.idx >= this.data.length) {
             return Promise.resolve({ value: undefined, done: true });
         }
+        
+        const awaitable_promise = Promise.resolve({value: this.data[this.idx], done:false});
         this.idx += 1;
-        return Promise.resolve({value: this.data[this.idx], done:false});
+        return awaitable_promise;
     }
 
     byPage: (settings?: PageSettings) => AsyncIterableIterator<Event[]>;
@@ -55,33 +60,36 @@ class EventOps implements EventsOperations {
 dotenv.config({path: path.join(__dirname, "test-data/.env")});
 const appconfig = AppConfig.loadFromEnvVar(undefined);
 
+const tenant_name = 'TechPass'
 
-describe('deployNewMarketContract', () => { 
-    test("test case 1", async () => {
+// test case 1
+// conditions:
+// - issue is Active
+// - southeast asia region issue is still Active
+// - central us region issue is Resolved
+// result:
+// - issue count = 1
+test("issue_Active_SEA_Active", async () => {
 
-        const data = test_data_case_1;
+    const data = test_data_case_1;
 
-        const result_iterator = new EventIterator(data);
+    //ts-mockito
+    const mrh :MicrosoftResourceHealth = mock(MicrosoftResourceHealth);
+    const mrh_instance = instance(mrh);
+    when(mrh.eventsOperations).thenReturn(new EventOps(data));
+    when(mrh.subscriptionId).thenCall(() => 'xxx-xx-xxx');
 
-        const mrh :MicrosoftResourceHealth = mock(MicrosoftResourceHealth);
-        const mrh_instance = instance(mrh);
 
-        when(mrh.eventsOperations).thenReturn(new EventOps(data));
+    const subscriptions = [
+        new Subscription('xxx-xx-xxx', 'sub-1')
+    ]
 
-        const apif = new ApiIssueFetcher('WOG', mrh_instance, 
-            [
-                new Subscription('xxx-xx-xxx', 'sub-1')
-            ], 
-            appconfig);
-        
-        
-        const issues: ServiceIssue[] = await apif.fetchIssuesAndImpactedResources();
-
+    const apif = new ApiIssueFetcher(tenant_name, mrh_instance, subscriptions, appconfig);
     
-        expect(2).toEqual(2);
-     });
-         
-});
+    const issues: ServiceIssue[] = await apif.fetchIssuesAndImpactedResources();
+
+    expect(issues.length).toEqual(1);
+ });
 
 
 // test case 2
